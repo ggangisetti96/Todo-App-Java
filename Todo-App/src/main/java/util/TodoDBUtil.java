47e28b88514ae1a9525db64ba5531d70a28fb35d
Gopinath Gangisetti <ggangisetti@unomaha.edu>
2022-03-16 16:26:33 -0500

    Added DB util file

--- Todo-App/src/main/java/util/TodoDBUtil.java
+++ Todo-App/src/main/java/util/TodoDBUtil.java
@@ -1,8 +1,17 @@
 package util;
 
+import datamodel.Todo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class TodoDBUtil {
 	static SessionFactory sessionFactory = null;
@@ -17,5 +26,42 @@ public class TodoDBUtil {
 	      return sessionFactory;
 	   }
 	   
-	   
+	   public static List<Todo> listTodos() {
+		      List<Todo> resultList = new ArrayList<Todo>();
+
+		      Session session = getSessionFactory().openSession();
+		      Transaction tx = null;  // each process needs transaction and commit the changes in DB.
+
+		      try {
+		         tx = session.beginTransaction();
+		         List<?> todos = session.createQuery("FROM TodoList_Gangisetti").list();
+		         for (Iterator<?> iterator = todos.iterator(); iterator.hasNext();) {
+		        	 Todo todo = (Todo) iterator.next();
+		            resultList.add(todo);
+		         }
+		         tx.commit();
+		      } catch (HibernateException e) {
+		         if (tx != null)
+		            tx.rollback();
+		         e.printStackTrace();
+		      } finally {
+		         session.close();
+		      }
+		      return resultList;
+		   }
+	   public static void createTodo(String todo, boolean done) {
+		      Session session = getSessionFactory().openSession();
+		      Transaction tx = null;
+		      try {
+		         tx = session.beginTransaction();
+		         session.save(new Todo(todo, Boolean.valueOf(done)));
+		         tx.commit();
+		      } catch (HibernateException e) {
+		         if (tx != null)
+		            tx.rollback();
+		         e.printStackTrace();
+		      } finally {
+		         session.close();
+		      }
+		   }
 }
